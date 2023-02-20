66471ae6e09c5873962054c5b5ed800df7dbaf31
MilenaKozhuharova <milenka896@gmail.com>
2023-02-20 18:19:03 +0200

    Add remaining exercises from DataTypesAndVariablesMoreExercise

--- Programming Fundamentals/Lectures/DataTypesAndVariablesMoreExercise/src/P02FromLeftToTheRight.java
+++ Programming Fundamentals/Lectures/DataTypesAndVariablesMoreExercise/src/P02FromLeftToTheRight.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class P02FromLeftToTheRight {
+import java.util.Scanner;
+
+public class P02FromLeftToTheRight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int lines = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < lines; i++) {
+            String[] inputArr = scanner.nextLine().split(" ");
+            long firstNumber = Long.parseLong(inputArr[0]);
+            long secondNumber = Long.parseLong(inputArr[1]);
+
+            long currentNumber = Math.abs(Math.max(firstNumber, secondNumber));
+            long sumOfDigits = 0;
+            long currentDigit;
+
+            while (currentNumber > 0) {
+                currentDigit = (currentNumber % 10);
+                sumOfDigits += currentDigit;
+                currentNumber /= 10;
+            }
+
+            System.out.println(sumOfDigits);
+        }
+    }
 }
--- Programming Fundamentals/Lectures/DataTypesAndVariablesMoreExercise/src/P04RefactoringPrimeChecker.java
+++ Programming Fundamentals/Lectures/DataTypesAndVariablesMoreExercise/src/P04RefactoringPrimeChecker.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class P04RefactoringPrimeChecker {
+import java.util.Scanner;
+
+public class P04RefactoringPrimeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int currentNum = 2; currentNum <= n; currentNum++) {
+            boolean isPrime = true;
+            for (int divider = 2; divider <= Math.sqrt(currentNum); divider++) {
+                if (currentNum % divider == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            System.out.printf("%d -> %b%n", currentNum, isPrime);
+        }
+    }
 }
